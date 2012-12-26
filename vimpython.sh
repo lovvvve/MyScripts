fc25950bd3597b05aed82d691432e588436150f1
lovvvve <lovvvve@gmail.com>
2012-12-26 18:24:23 +0800

    小修改

--- vimpython.sh
+++ vimpython.sh
@@ -8,7 +8,7 @@ cat >$1<<EOF
 #!/bin/env python
 # -*- coding: utf-8 -*-
 #:Tital: `basename $1`
-#:synopsis: `basename $1`
+#:Synopsis: `basename $1`
 #:Date: `date +%F`
 #:Version: 1.0
 #:Author: lovvvve
